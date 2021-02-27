d840409bd6fd22359a711ba75f1ccab48bd4d510
rachel-taylor <taylor.d.rachel@gmail.com>
2021-02-27 00:20:52 -0800

    commit tree 997443e598f9ffc48564929904a9f05a1b066942

---
+++ index.js
@@ -0,0 +1,43 @@
+function wakeDog(dogName, dogBreed) {
+    let wake = (`Wake ${dogName} the ${dogBreed}`);
+    console.log(wake); 
+    return wake;
+  }
+
+  function leashDog(dogName, dogBreed) {
+    let leash = (`Leash ${dogName} the ${dogBreed}`);
+    console.log(leash);
+    return leash;
+  } 
+
+  function walkToPark(dogName, dogBreed) {
+    let walk = (`Walk to the park with ${dogName} the ${dogBreed}`);
+    console.log(walk);
+    return walk;
+  }  
+
+  function throwFrisbee(dogName, dogBreed) {
+    let fetch = (`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    console.log(fetch);
+    return fetch;
+  } 
+
+  function walkHome(dogName, dogBreed) {
+    let home = (`Walk home with ${dogName} the ${dogBreed}`);
+    console.log(home);
+    return home;
+  } 
+
+  function unleashDog(dogName, dogBreed) {
+    let unleash = (`Unleash ${dogName} the ${dogBreed}`);
+    console.log(unleash);
+    return unleash;
+  } 
+
+  var routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+  function exerciseDog(dogName, dogBreed){
+      
+  }
+
+ 
